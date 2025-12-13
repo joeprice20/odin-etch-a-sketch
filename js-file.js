4f18c5d072c18caac2b4da207e67484f2323e519
joeprice20 <19871383+joeprice20@users.noreply.github.com>
2025-12-13 19:25:19 +0000

    add grid

--- js-file.js
+++ js-file.js
@@ -1,1 +1,19 @@
-alert("bing bong")
+
+function createGrid(gridWidth) {
+    const gridContainer = document.querySelector("#grid-container");
+
+    for (let x = 0; x < gridWidth; x++) {
+        let gridColumn = document.createElement("div");
+        gridColumn.classList.add("grid-column");
+        gridContainer.appendChild(gridColumn)
+
+        for (let y = 0; y < gridWidth; y++) {
+            let gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cells");
+            gridColumn.appendChild(gridCell);
+        }
+
+    }
+}
+
+createGrid(4);
