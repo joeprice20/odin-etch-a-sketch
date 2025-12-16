cec662b840c013345bf75d77da0c3fadf9b77d90
joeprice20 <19871383+joeprice20@users.noreply.github.com>
2025-12-16 12:06:36 +0000

    Add variable grid size input

--- js-file.js
+++ js-file.js
@@ -1,25 +1,46 @@
 
-function onHover(e){
-    e.target.style.background = "blue"
+function onHover(e) {
+    e.target.style.background = "blue";
+}
+
+function deleteChildren(parentElement) {
+
+    //e.firstElementChild can be used. 
+    let child = parentElement.lastElementChild;
+    while (child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
 }
 
 
 function createGrid(gridWidth) {
     const gridContainer = document.querySelector("#grid-container");
 
+    deleteChildren(gridContainer)
+
     for (let x = 0; x < gridWidth; x++) {
         let gridColumn = document.createElement("div");
         gridColumn.classList.add("grid-column");
-        gridContainer.appendChild(gridColumn)
+        gridContainer.appendChild(gridColumn);
 
         for (let y = 0; y < gridWidth; y++) {
             let gridCell = document.createElement("div");
             gridCell.classList.add("grid-cells");
-            gridCell.addEventListener("mouseover", onHover)
+            gridCell.addEventListener("mouseover", onHover);
             gridColumn.appendChild(gridCell);
         }
 
     }
 }
+
+function onGenerateGridButtonClicked() {
+    const gridSizeInput = document.querySelector("#grid-size-input");
+    console.log(gridSizeInput.value);
+    createGrid(parseInt(gridSizeInput.value));
+}
+
+const generateGridButton = document.querySelector("#generate-grid-button");
+generateGridButton.addEventListener("click", onGenerateGridButtonClicked);
 
 createGrid(16);
